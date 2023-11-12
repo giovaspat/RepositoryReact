08f0dd5dd501ec840fe362a6f2a0955f0000c8bb
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2023-11-12 03:29:08 +0100

    Update AlertClock.jsx

--- AlertClock.jsx
+++ AlertClock.jsx
@@ -1,15 +1,3 @@
-export function AlertClock () {
-  function handleButtonClick () {
-    const currentTime = new Date;
-    alert(`${currentTime.toLocaleTimeString()}`)
-  }
-
-  return (
-  <div>
-    <button onClick={handleButtonClick}>Click here</button>
-  </div>
-  )
-}
 
 
  
