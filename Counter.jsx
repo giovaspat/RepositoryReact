b53f3de7115c0c44d178c564e6e3d90222bc507f
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2023-11-22 21:22:18 +0100

    Update Counter.jsx

--- Counter.jsx
+++ Counter.jsx
@@ -1,26 +1,3 @@
-import { useState } from "react";
-import { CounterDisplay } from "./CounterDisplay";
 
-export function Counter({ startValue = 0 }) {
-  const [counter, setCounter] = useState(startValue);
-
-  function handleIncrement() {
-    setCounter((counter) => counter + 1);
-  }
-
-  function handleDecrement() {
-    setCounter((counter) => counter - 3);
-  }
-
-  function handleReset() {
-    setCounter(startValue);
-  }
-
-  return (
-    <CounterDisplay counter={counter} handleIncrement={() => handleIncrement(counter + 1)}
-      handleDecrement={handleDecrement} handleReset={handleReset}
-    />
-  );
-}
 
 
