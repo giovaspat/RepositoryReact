95266359e59093377c604bed9ffe086297f5abc4
Giovanni Spatafora <141187369+giovaspat@users.noreply.github.com>
2023-12-01 01:03:25 +0100

    Add files via upload

---
+++ src/GithubUser.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useGithubUser } from "./useGithubUser";
+
+export function GithubUser({ username }) {
+  const [isLoading, setLoading] = useState(false);
+  const { data, error, onRecover } = useGithubUser(username);
+
+  function handleGetUserData() {
+    setLoading(true);
+  }
+
+  return (
+    <div>
+      <button onClick={onRecover}>Recover Data</button> 
+      <button onClick={handleGetUserData}>Load Data</button>
+      {isLoading && error && <h2>An error has occurred</h2>}
+      {isLoading && data && !error && (
+        <div>
+          <h1>{data.name}</h1>
+          <p>{data.login}</p>
+          <p>{data.avatar_url}</p>
+        </div>
+      )}
+    </div>
+  );
+}
